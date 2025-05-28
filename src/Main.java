629a4d77ee319e69c673f395e20babe4933df536
Johannes Holley <147592122+holleyj@users.noreply.github.com>
2025-05-28 22:48:20 +0200

    optimized imports

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
